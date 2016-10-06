d4dcb2b73478949a297a2c998d9e0942ac026ccb
santosh54321 <Arrojusachinteam@gmail.com>
2016-10-06 19:52:39 +0530

    Assessment 4 done

---
+++ Game/Game/src/Main.java
@@ -0,0 +1,90 @@
+import java.util.List;
+import java.io.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws Exception {
+		
+	   BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+
+        
+
+        Player player = new Player("Fred", 100);
+      
+        int totalWins = 0;
+        int totalLosses = 0;
+
+        while (true)
+        {
+            int winCount = 0;
+            int loseCount = 0;
+            //to remove bug not start from 1, change i=0 to i=1and add = operator
+            for (int i = 1; i <= 100; i++)
+            {
+               
+
+            	String name = "Fred";
+            	int balance = 100;
+            	int limit = 0;
+                player = new Player(name, balance);
+                player.setLimit(limit);
+                int bet = 5;
+
+                System.out.println(String.format("Start Game %d: ", i));
+                System.out.println(String.format("%s starts with balance %d, limit %d", 
+                		player.getName(), player.getBalance(), player.getLimit()));
+
+                int turn = 0;
+                while (player.balanceExceedsLimitBy(bet) && player.getBalance() < 200)
+                {
+                    turn++;   
+                //for removing bug of showing same rolled on every turn initialise here
+                //not outside the while loop
+                Dice d1 = new Dice();
+                Dice d2 = new Dice();
+                Dice d3 = new Dice();
+                Game game = new Game(d1, d2, d3);
+                List<DiceValue> cdv = game.getDiceValues();
+                	DiceValue pick = DiceValue.getRandom();
+                   
+                	System.out.printf("Turn %d: %s bet %d on %s\n",
+                			turn, player.getName(), bet, pick); 
+                	
+                	int winnings = game.playRound(player, pick, bet);
+                    
+                    
+                    System.out.printf("Rolled %s, %s, %s\n",
+                    		cdv.get(0), cdv.get(1), cdv.get(2));
+                    
+                    if (winnings > 0) {
+	                    System.out.printf("%s won %d, balance now %d\n\n",
+	                    		player.getName(), winnings, player.getBalance());
+	                	winCount++; 
+                    }
+                    else {
+	                    System.out.printf("%s lost, balance now %d\n\n",
+	                    		player.getName(), player.getBalance());
+	                	loseCount++;
+                    }
+                    
+                } //while
+
+                System.out.print(String.format("%d turns later.\nEnd Game %d: ", turn, i));
+                System.out.println(String.format("%s now has balance %d\n", player.getName(), player.getBalance()));
+                
+            } //for
+            
+            System.out.println(String.format("Win count = %d, Lose Count = %d, %.2f", winCount, loseCount, (float) winCount/(winCount+loseCount)));
+            totalWins += winCount;
+            totalLosses += loseCount;
+            //to remove bug enter a exit or restart message
+            System.out.print("Press 'q' for quit or press any key to restart the Game :");
+            String ans = console.readLine();
+            if (ans.equals("q"))break;
+        } //while true
+        
+        System.out.println(String.format("Overall win rate = %.1f%%", (float)(totalWins * 100) / (totalWins + totalLosses)));
+	 System.out.println("*****************************************"+"\n Thank You! for playing the Game"+"\n*****************************************");
+        }
+
+}
